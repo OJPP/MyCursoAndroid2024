d708737a0a38cfb8b09628f25d4c965a6d608786
Octavio Paulo <octavio.j.p.paulo@gmail.com>
2024-01-27 11:30:42 +0000

    Novo projecto: AppGasEta

---
+++ AppGasEta/app/src/main/java/com/ojpp/devandroid/appgaseta/model/Curso.java
@@ -0,0 +1,22 @@
+package com.ojpp.devandroid.appgaseta.model;
+
+public class Curso {
+
+    private String nomeDoCursoDesejado;
+
+    public Curso() {
+    }
+
+    public Curso(String nomeDoCursoDesejado) {
+        this.nomeDoCursoDesejado = nomeDoCursoDesejado;
+    }
+
+    public String getNomeDoCursoDesejado() {
+        return nomeDoCursoDesejado;
+    }
+
+    public void setNomeDoCursoDesejado(String nomeDoCursoDesejado) {
+        this.nomeDoCursoDesejado = nomeDoCursoDesejado;
+    }
+
+}
---
+++ AppGasEta/app/src/main/java/com/ojpp/devandroid/appgaseta/view/MainActivity.java
@@ -0,0 +1,112 @@
+package com.ojpp.devandroid.appgaseta.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.ojpp.devandroid.appgaseta.R;
+import com.ojpp.devandroid.appgaseta.controller.CursoController;
+import com.ojpp.devandroid.appgaseta.controller.PessoaController;
+import com.ojpp.devandroid.appgaseta.model.Pessoa;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    PessoaController pessoaController;
+    CursoController cursoController;
+    EditText editPrimeiroNome;
+    EditText editSobrenome;
+    EditText editNomeCurso;
+    EditText editTelefoneContato;
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinaliar;
+    Spinner spinner;
+    Pessoa pessoa;
+    List<String> nomeDosCursos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinner);
+
+        pessoaController = new PessoaController(MainActivity.this);
+        pessoaController.toString();
+
+        Pessoa pessoa = pessoaController.buscar();
+
+        editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
+        editSobrenome = findViewById(R.id.editSobrenome);
+        editNomeCurso = findViewById(R.id.editNomeCurso);
+        editTelefoneContato = findViewById(R.id.editTelefoneContato);
+
+        editPrimeiroNome.setText(pessoa.getPrimeiroNome());
+        editSobrenome.setText(pessoa.getSobrenome());
+        editNomeCurso.setText(pessoa.getCursoDesejado());
+        editTelefoneContato.setText(pessoa.getTelefoneContacto());
+
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnFinaliar = findViewById(R.id.btnFinaliar);
+
+        // Adapter
+        // Layout
+        // Injetar o Adapter ao Spinner
+        cursoController = new CursoController();
+        nomeDosCursos = cursoController.dadosParaSpinner();
+
+        spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                cursoController.dadosParaSpinner());
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+        spinner.setAdapter(adapter);
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editPrimeiroNome.setText("");
+                editSobrenome.setText("");
+                editNomeCurso.setText("");
+                editTelefoneContato.setText("");
+
+                pessoaController.limpar();
+            }
+        });
+
+        btnFinaliar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Volte sempre", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pessoa.setPrimeiroNome(editPrimeiroNome.getText().toString());
+                pessoa.setSobrenome(editSobrenome.getText().toString());
+                pessoa.setCursoDesejado(editNomeCurso.getText().toString());
+                pessoa.setTelefoneContacto(editTelefoneContato.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Salvo " + pessoa.toString(), Toast.LENGTH_LONG).show();
+
+                pessoaController.salvar(pessoa);
+            }
+        });
+
+        Log.i("POOAndroid", pessoa.toString());
+    }
+
+}
